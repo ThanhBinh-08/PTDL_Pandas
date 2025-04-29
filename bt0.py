285c0a11cdc4424738d3bcf18e12b6048fdcba78
ThanhBinh-08 <thanhbin26th8@gmail.com>
2025-04-29 10:55:59 +0700

    Add 17 new Pandas exercises

---
+++ bt0.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 11 10:40:12 2025
+
+@author: DELL
+"""
+import pandas as pd 
+
+#Tạo một bảng dataframe  chứa các dữ liêu XYZ
+df = pd.DataFrame({'X':[78,85,96,80,86], 'Y':[84,94,89,83,86],'Z':[86,97,96,72,83]}); # trong đó {'dòng':'cột'}
+
+#output
+print(df);
---
+++ bt1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 11 16:24:07 2025
+
+@author: DELL
+"""
+
+#gọi thư viện pandas và numpy
+import pandas as pd
+import numpy as np
+
+#Viết chương trình Pandas để tạo và hiển thị DataFrame từ dữ liệu từ điển được chỉ định có nhãn chỉ mục.
+
+#Dữ liệu cho sẵn
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+
+#List labels thay thành index cho từng dòng → thay vì mặc định là 0,1,2,...
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+#Tạo dataframe 
+df = pd.DataFrame(exam_data,index=labels)
+
+#output
+print(df)
